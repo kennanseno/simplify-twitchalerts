25a380ee33aa3dbc44a92960c31edd30256ac773
kennanseno <Kennan.Seno@mastercard.com>
2016-07-14 12:02:33 +0100

    design form

--- public/components/controllers/HomeController.js
+++ public/components/controllers/HomeController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mostPopularListingsApp.home', ['ngRoute', 'ngMaterial', 'ngCookies'])
+angular.module('mostPopularListingsApp.home', ['ngRoute', 'ngMaterial', 'ngCookies', 'ngMessages'])
 
 // Routing configuration for this module
 .config(['$routeProvider',function($routeprovider){
